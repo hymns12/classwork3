9c0ce151d528181ad5c9046932b9a0a62b2e9346
hymns12 <nickcloudsyinka1@gmail.com>
2024-02-23 18:37:06 -0500

    updata

--- contracts/benfish.sol
+++ contracts/benfish.sol
@@ -1,31 +1,63 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.9;
 
-contract bernfhisry {
-    address owner;
+contract Vault {
+    uint256 private id;
 
-    mapping (address => uint) balancesOf;
-    mapping (address => uint) benfhsiar;
-
-    event Success(address indexed from, address to);
-
-
-    constructor() {
-        owner = msg.sender;
+    struct Grant {
+        string name;
+        uint256 amount;
+        uint256 duration;
+        uint256 id;
     }
 
-    function depets(uint _reciever) external payable {
-        require(msg.sender != address(0), "address ZERO not valid");
-        require(msg.value > 0, "must be greater than ZERO");
+    mapping(address => uint256) public grantIdCount;
+    mapping(address => mapping(uint256 => Grant)) public individualGrantToBeneficiary;
+    mapping(address => bool) public hasGrant;
+    address[] public beneficiaries;
 
-        balancesOf[msg.sender] += msg.value;
-        benfhsiar[msg.sender] = _reciever;
-
-    } 
-
-    function genrat(uint _reciever) external {
-       require(balancesOf[msg.sender] > 0, "you cant send any token");
-        benfhsiar[msg.sender] = _reciever;
-
+    function noZeroAddr() private view {
+        if (msg.sender == address(0)) {
+            revert("no zero address");
+        }
     }
+
+    function storeBeneficiary(address _recipient) private {
+        if (!hasGrant[_recipient]) {
+            beneficiaries.push(_recipient);
+            hasGrant[_recipient] = true;
+        }
+    }
+
+    function giveGrant(address _recipient, string memory _grantName, uint256 _duration) external payable {
+        noZeroAddr();
+
+        uint256 _id = id + 1;
+
+        payable(address(this)).transfer(msg.value);
+
+        Grant memory grant = Grant(_grantName, msg.value, _duration, _id);
+        individualGrantToBeneficiary[_recipient][_id] = grant;
+
+        grantIdCount[_recipient] = grantIdCount[_recipient] + 1;
+        storeBeneficiary(_recipient);
+        id = _id;
+    }
+
+    function claimGrant(uint _id) external payable {
+        noZeroAddr();
+
+        Grant memory grant = individualGrantToBeneficiary[msg.sender][_id];
+        require(block.timestamp >= grant.duration, "wait first");
+
+        uint256 amount = grant.amount;
+        grant.amount = 0;
+        payable(msg.sender).transfer(amount);
+
+        grantIdCount[msg.sender] = grantIdCount[msg.sender] - 1;
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
 }
